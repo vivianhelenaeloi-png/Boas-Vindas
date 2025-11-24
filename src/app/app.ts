32ce51be304847aff9b7624c757deff68b4f2be4
Vivian <vivianhelenaeloi@gmail.com>
2025-11-24 15:12:04 +0000

    Criação dos componentes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,4 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
-})
-export class App {
-  protected readonly title = signal('primeiroprojeto');
-}
+@Component
